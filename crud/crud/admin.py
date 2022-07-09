00fe2740566869f594d3a6e9157f564f80fb89f0
Shivam Singh <51095912+codewithshiva@users.noreply.github.com>
2022-07-09 18:59:40 +0530

    connected postgresql

--- crud/crud/admin.py
+++ crud/crud/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from crud.models import MoviesModel
+from crud.models import GenreModel
 
 class MoviesAdmin(admin.ModelAdmin):
+    pass
+class GenreAdmin(admin.ModelAdmin):
     pass
 
 
 admin.site.register(MoviesModel,MoviesAdmin)
+admin.site.register(GenreModel,GenreAdmin)
--- crud/crud/models.py
+++ crud/crud/models.py
@@ -1,14 +1,29 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 import datetime
 
 
+class GenreModel(models.Model):
+    Genre=models.CharField(max_length=60,primary_key=True)
+    
+    def __str__(self):
+        return self.Genre or ''
 class MoviesModel(models.Model):
+    def validate_Ratings(value):
+        if value >10:
+            raise ValidationError(
+            _('Ratings cannot be more than 10'),
+            params={'value': value},
+        )
     id=models.AutoField(primary_key=True)
     Title=models.CharField(max_length=60,blank=False,validators=[MinLengthValidator(3)])
     ReleaseDate=models.DateField(default=datetime.date.today)
-    Genre=models.ForeignKey('self', on_delete=models.CASCADE)
-    Price=models.DecimalField(max_digits=5, decimal_places=2)
-    Ratings=models.DecimalField(max_digits=2, decimal_places=1)
+    genre=models.ForeignKey('GenreModel',blank=False, on_delete=models.CASCADE)
+    Price=models.DecimalField(max_digits=10, decimal_places=2)
+    Ratings=models.DecimalField(max_digits=3, decimal_places=1,validators=[validate_Ratings])
+    
     def __str__(self):
-        return self.Title or ''
+        return self.Title
+    
